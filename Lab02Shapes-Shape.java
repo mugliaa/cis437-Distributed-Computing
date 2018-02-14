fdbfebb4424a69deebdc86fdd26895aeea6d9c38
Adam Muglia <mugliaa@mail.gvsu.edu>
2018-02-14 10:20:30 -0500

    Create Lab02Shapes-Shape.java

---
+++ Lab02Shapes-Shape.java
@@ -0,0 +1,12 @@
+/**
+ * Abstract interface of a Shape object.
+ * @author Jonathan Engelsma
+ * 
+ * Name: Adam Muglia
+ * Assignment: Lab 02
+ * Date: 01/17/2018
+ * 
+ */
+public interface Shape {
+	public double computeArea();
+}
